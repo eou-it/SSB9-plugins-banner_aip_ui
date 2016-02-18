1f8655d70efd464999c215922b80f2cfb2270644
Jin Woo Shin <cootang2@gmail.com>
2016-02-18 16:15:41 -0700

    modified return json

--- grails-app/conf/BannerCsrUiResources.groovy
+++ grails-app/conf/BannerCsrUiResources.groovy
@@ -8,14 +8,14 @@
 
 modules = {
     'csr-angular' { // Temp resources
-//        resource url:[plugin: 'banner-csr-ui', file: 'node_modules/angular/angular.js']
-//        resource url:[plugin: 'banner-csr-ui', file: 'node_modules/angular-route/angular-route.js']
-//        resource url:[plugin: 'banner-csr-ui', file: 'node_modules/angular-animate/angular-animate.js']
-//        resource url:[plugin: 'banner-csr-ui', file: 'node_modules/angular-ui-router/release/angular-ui-router.js']
-//        resource url:[plugin: 'banner-csr-ui', file: 'node_modules/angular-sanitize/angular-sanitize.js']
-//        resource url:[plugin: 'banner-csr-ui', file: 'node_modules/angular-messages/angular-messages.js']
-//        resource url:[plugin: 'banner-csr-ui', file: 'node_modules/angular-resource/angular-resource.js']
-//        resource url:[plugin: 'banner-csr-ui', file: 'node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js']
+        resource url:[plugin: 'banner-csr-ui', file: 'node_modules/angular/angular.js']
+        resource url:[plugin: 'banner-csr-ui', file: 'node_modules/angular-route/angular-route.js']
+        resource url:[plugin: 'banner-csr-ui', file: 'node_modules/angular-animate/angular-animate.js']
+        resource url:[plugin: 'banner-csr-ui', file: 'node_modules/angular-ui-router/release/angular-ui-router.js']
+        resource url:[plugin: 'banner-csr-ui', file: 'node_modules/angular-sanitize/angular-sanitize.js']
+        resource url:[plugin: 'banner-csr-ui', file: 'node_modules/angular-messages/angular-messages.js']
+        resource url:[plugin: 'banner-csr-ui', file: 'node_modules/angular-resource/angular-resource.js']
+        resource url:[plugin: 'banner-csr-ui', file: 'node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js']
 //        resource url:[plugin: 'banner-csr', file: 'bower_components/angular-common/build/angular-common.js']
         resource url:[plugin: 'banner-csr-ui', file: 'node_modules/angular-aria/angular-aria.js']
         resource url:[plugin: 'banner-csr-ui', file: 'node_modules/moment/moment.js']
@@ -24,14 +24,15 @@ modules = {
         resource url:[plugin: 'banner-csr-ui', file: 'csrApp/csrCheck.js']
     }
     'bannerCSRUI' {
-        dependsOn 'bannerWebLTR, bootstrap'
+        dependsOn 'csr-angular'
+        dependsOn 'bannerSelfServiceCommonLTR, extensibilityCommon, extensibilityAngular, common-components, angularApp, bootstrap'
         dependsOn 'bannerCSRApp'
         resource url:[plugin: 'banner-csr-ui', file: 'css/xe-ui-components.css']
         resource url:[plugin: 'banner-csr-ui', file: 'css/csr-main.css']
     }
     'bannerCSRUIRTL' {
         dependsOn 'bannerCSRUI'
-        dependsOn 'bannerWebRTL'
+        dependsOn 'bannerSelfServiceCommonRTL, extensibilityAngularRTL'
         resource url:[plugin: 'banner-ui-ss', file: 'bootstrap/css/bootstrap-rtl.css']
         resource url:[plugin: 'banner-csr-ui', file: 'css/xe-ui-components-rtl.css']
         resource url:[plugin: 'banner-csr-ui', file: 'css/csr-main-rtl.css']
@@ -41,7 +42,7 @@ modules = {
         resource url:[plugin: 'banner-csr-ui', file: 'csrApp/utils/register.js']
     }
     'bannerCSRApp' {
-        dependsOn 'csr-angular, bannerCSRUtils'
+        dependsOn 'bannerCSRUtils'
         resource url:[plugin: 'banner-csr-ui', file: 'csrApp/app.js']
         resource url:[plugin: 'banner-csr-ui', file: 'csrApp/admin/adminLandingPageCtrl.js']
         resource url:[plugin: 'banner-csr-ui', file: 'csrApp/admin/listActionItem/adminListItemPageCtrl.js']
