0ae7bb49a51e07a42056ddda41d922e3fe071bd7
Craig Riddle <craig.riddle@ellucian.com>
2016-04-01 22:01:41 +0100

    reverting

--- grails-app/taglib/net/hedtech/banner/csr/CsrTagLib.groovy
+++ grails-app/taglib/net/hedtech/banner/csr/CsrTagLib.groovy
@@ -6,10 +6,7 @@ import org.springframework.context.i18n.LocaleContextHolder
 class CsrTagLib {
     def i18n_setup = { attrs ->
         def map = [:]
-        //grailsApplication.mainContext.getBean('messageSource').getMergedProperties(LocaleContextHolder.getLocale()).properties.each { key ->
-        //    map.put key.key, key.value
-        //}
-        applicationContext.getBean("messageSource").getMergedProperties(LocaleContextHolder.getLocale()).properties.each { key ->
+        grailsApplication.mainContext.getBean('messageSource').getMergedProperties(LocaleContextHolder.getLocale()).properties.each { key ->
             map.put key.key, key.value
         }
         out << "window.i18n = ${map as JSON};\n"
