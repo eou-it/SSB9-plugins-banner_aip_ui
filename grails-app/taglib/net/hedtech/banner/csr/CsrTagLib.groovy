8a6c13625faf67624dc0932fa583c01a572fb0d9
Craig Riddle <craig.riddle@ellucian.com>
2016-04-01 21:08:06 +0100

    i18n experiment

--- grails-app/taglib/net/hedtech/banner/csr/CsrTagLib.groovy
+++ grails-app/taglib/net/hedtech/banner/csr/CsrTagLib.groovy
@@ -1,19 +1,21 @@
 package net.hedtech.banner.csr
 
 import grails.converters.JSON
-import grails.util.Holders
 import org.springframework.context.i18n.LocaleContextHolder
 
 class CsrTagLib {
     def i18n_setup = { attrs ->
         def map = [:]
-        Holders.grailsApplication.mainContext.getBean('messageSource').getMergedProperties(LocaleContextHolder.getLocale()).properties.each { key ->
+        //grailsApplication.mainContext.getBean('messageSource').getMergedProperties(LocaleContextHolder.getLocale()).properties.each { key ->
+        //    map.put key.key, key.value
+        //}
+        applicationContext.getBean("messageSource").getMergedProperties(LocaleContextHolder.getLocale()).properties.each { key ->
             map.put key.key, key.value
         }
         out << "window.i18n = ${map as JSON};\n"
     }
     def csrVersion = { attrs ->
-        def plugin = Holders.applicationContext.getBean('pluginManager').allPlugins.find {
+        def plugin = applicationContext.getBean('pluginManager').allPlugins.find {
             it -> it.name == "bannerCsrUi"
         }
         def map = [
