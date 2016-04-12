85e8ce9bffdcc2cc534ed00b6f54fa88b1275d40
Jin Woo Shin <cootang2@gmail.com>
2016-04-12 13:15:38 -0600

    urlmapping fix

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,7 +1,6 @@
 class UrlMappings {
 
 	static mappings = {
-        "ssb/csr/$action/$params*" (controller:"csr")
         "/$controller/$action?/$id?(.$format)?"{
             constraints {
                 // apply constraints here
