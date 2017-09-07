d91983c31c5b9d1c282d6ecda87c56fce8b52ca6
Jeevan M <JMahadevaiah@HIBAIL51954.ad.harman.com>
2017-09-07 19:08:04 +0530

    Manage folder card text modifications

--- web-app/aipApp/common/directives/admin/landing-item/js/landingItem.js
+++ web-app/aipApp/common/directives/admin/landing-item/js/landingItem.js
@@ -10,10 +10,11 @@ angular.module('bannerAIPUI')
                 model: "=",
                 method: "&"
             },
-            controller: ['$scope', '$attrs', "$state","$http","BCM_ROOT", function($scope, $attrs, $state,$http,BCM_ROOT) {
+            controller: ['$scope', '$attrs', "$state","$http","$filter","BCM_ROOT", function($scope, $attrs, $state,$http,$filter,BCM_ROOT) {
                 if (!$scope.model) {
                     devErrorMessages += "model attribute is required\n";
                 }
+                $scope.model.filteredDescription = $filter('i18n_aip')($scope.model.description).split("//br");
                 $scope.changeState = function(state) {
                     $http.get(BCM_ROOT + "/BCM/getBCMLocation").then(function (response) {
                         $scope.bcm = response.data.bcmURL;
