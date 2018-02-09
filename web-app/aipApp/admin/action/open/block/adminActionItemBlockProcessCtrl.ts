0dba1408ccf284de5ebbd311f184fa82ac460abd
Jeevan M <JMahadevaiah@HIBAIL51954.ad.harman.com>
2018-02-09 11:59:38 +0530

    block process functionality done

--- web-app/aipApp/admin/action/open/block/adminActionItemBlockProcessCtrl.ts
+++ web-app/aipApp/admin/action/open/block/adminActionItemBlockProcessCtrl.ts
@@ -266,38 +266,38 @@ module AIP {
 
 
 
-                if (this.originalAssign.length>0 && this.isEqual(this.selected, this.originalAssign)) {
-                    validation = false;
-                }
-                if (unassigned.length !== 0) {
-                    validation = false;
-                }
-                return validation;
+            if (this.originalAssign.length>0 && this.isEqual(this.selected, this.originalAssign)) {
+                validation = false;
+            }
+            if (unassigned.length !== 0) {
+                validation = false;
+            }
+            return validation;
 
         }
 
         /*selectActionItem(item, index) {
-            var currentAssigned = this.assignedActionItems[index];
-            if (currentAssigned.actionItemId === item.actionItemId) {
-                return;
-            }
-            if (!currentAssigned.actionItemId) {
-                currentAssigned.sequenceNumber = index + 1;
-            }
-            currentAssigned.actionItemId = item.actionItemId;
-            currentAssigned.actionItemFolderName = item.folderName;
-            currentAssigned.actionItemName = item.actionItemName;
-            currentAssigned.actionItemStatus = item.actionItemStatus;
-            this.assignedActionItems[index] = currentAssigned;
-            if(!this.selected[index].actionItemId) {
-                item.seq = index + 1;
-                this.selected[index] = item;
-            }
-            this.selected = this.selected.filter((item, idx) => {
-                return true;
-            });
-            this.reAssignSeqnumber()
-        }*/
+         var currentAssigned = this.assignedActionItems[index];
+         if (currentAssigned.actionItemId === item.actionItemId) {
+         return;
+         }
+         if (!currentAssigned.actionItemId) {
+         currentAssigned.sequenceNumber = index + 1;
+         }
+         currentAssigned.actionItemId = item.actionItemId;
+         currentAssigned.actionItemFolderName = item.folderName;
+         currentAssigned.actionItemName = item.actionItemName;
+         currentAssigned.actionItemStatus = item.actionItemStatus;
+         this.assignedActionItems[index] = currentAssigned;
+         if(!this.selected[index].actionItemId) {
+         item.seq = index + 1;
+         this.selected[index] = item;
+         }
+         this.selected = this.selected.filter((item, idx) => {
+         return true;
+         });
+         this.reAssignSeqnumber()
+         }*/
 
 
 
@@ -415,8 +415,12 @@ module AIP {
         }
 
         saveSucess(){
+
             var saveData = this.selected.map((item)=>
             {
+                if(item.process.personAllowed==="N"){
+                    item.persona="";
+                }
                 return {processId:item.process.id, persona: item.persona};
             });
             this.adminActionService.updateBlockedProcessItems(this.$state.params.actionItemId,this.globalBlockProcess,saveData)
