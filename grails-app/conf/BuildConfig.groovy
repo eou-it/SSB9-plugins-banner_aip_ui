4618406b75b52e3fb62536c99d3cb25457404898
Jeevan M <JMahadevaiah@HIBAIL51954.ad.harman.com>
2018-02-15 11:40:10 +0530

    RTE issue check

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -41,7 +41,7 @@ grails.project.dependency.resolution = {
     dependencies {
     }
 
-/*    plugins {
+    plugins {
 // TODO: evaluate if these are really needed
 //        runtime ':font-awesome-resources:5.0.0.0'
         runtime "org.grails.plugins:font-awesome-resources:4.4.0"
@@ -50,8 +50,8 @@ grails.project.dependency.resolution = {
 //        compile ':cached-resources:1.0'
 //        compile ':cache-headers:1.1.5'
         test ':code-coverage:2.0.3-3'
-        compile ":ckeditor:4.5.4.1"
-    }*/
+        /*compile ":ckeditor:4.5.4.1"*/
+    }
 
 
 }
