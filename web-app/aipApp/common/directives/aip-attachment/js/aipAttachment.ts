9f3e63fab78de0765b117aa40aeca1d9f38cab0c
Krishna Ragipani <krishna.ragipani@ellucian.com>
2018-08-28 00:34:28 +0530

    CSAT-4872 Created new directive for displaying content of modal window

---
+++ web-app/aipApp/common/directives/aip-attachment/js/aipAttachment.ts
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ Copyright 2018 Ellucian Company L.P. and its affiliates.
+ ********************************************************************************/
+///<reference path="../../../../../typings/tsd.d.ts"/>
+
+declare var register;
+
+module AIPUI {
+    export class AIPAttachment{
+
+        templateUrl: string;
+        restrict: string;
+        scope:any;
+        replace: boolean;
+        constructor() {
+            this.restrict = "AE";
+            this.replace = false;
+            this.scope = {
+            }
+        }
+        compile() {
+
+        }
+        link(scope, elem, attr) {
+
+        }
+        controller($scope) {
+
+        }
+
+    }
+}
+
+register("bannerAIPUI").directive("aipAttachment", AIPUI.AIPAttachment);
