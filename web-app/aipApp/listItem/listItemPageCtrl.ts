9cb0a9ce6c86f8b478354b96a838a1945ac63000
Supritha Kulkarni <supritha.shrikantkulkarni@ellucian.com>
2018-09-20 00:15:10 +0530

    BS-7170 Same attachments grid shown for all responses. Fixed this issue.

--- web-app/aipApp/listItem/listItemPageCtrl.ts
+++ web-app/aipApp/listItem/listItemPageCtrl.ts
@@ -351,7 +351,7 @@ module AIP {
                     "src='../images/attach_icon_disabled.svg' title = 'Click to add documents' " +
                     "class=' pb-detail pb-item pb-paperclip'/>");
 
-                window.params.responseId = responseId;
+
                 window.params.maxAttachments = allowedAttachments;
                 responseElement.after(paperClipElement);
 
@@ -361,6 +361,7 @@ module AIP {
                     currentId = "#pbid-ActionItemStatusAgree-radio-0-" + currentId;
                     if ($(currentId)[0].checked === true) {
                         //make sure paper clip is enabled
+                        window.params.responseId = $(currentId)[0].value;
                         $("#" + selectedPaperClip)[0].setAttribute("src", "../images/attach_icon_default.svg");
                         var evt = new CustomEvent('responseChanged');
                         window.dispatchEvent(evt);
