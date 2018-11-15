51b7395dac7cadad6977552e6b04173b4f71c493
Krishna Ragipani <krishna.ragipani@ellucian.com>
2018-11-15 18:15:52 +0530

    BS-7412 AIP: Reviewer reviews a document already uploaded for an Action Item

--- web-app/aipApp/aipReview/reviewActionItem/reviewActionItemCtrl.js
+++ web-app/aipApp/aipReview/reviewActionItem/reviewActionItemCtrl.js
@@ -6,9 +6,9 @@
 ///<reference path="../../common/services/userService.ts"/>
 var AIP;
 (function (AIP) {
-    var ReviewActionItemCtrl = (function () {
-        function ReviewActionItemCtrl($scope, $state, AIPReviewService, AIPUserService, SpinnerService, $timeout, $q, $uibModal, APP_ROOT, $sce) {
-            this.$inject = ["$scope", "$state", "AIPReviewService", "AIPUserService", "SpinnerService", "$timeout", "$q", "$uibModal", "APP_ROOT", "$sce"];
+    var ReviewActionItemCtrl = /** @class */ (function () {
+        function ReviewActionItemCtrl($scope, $state, AIPReviewService, AIPUserService, SpinnerService, $timeout, $q, $uibModal, APP_ROOT, $sce, $filter) {
+            this.$inject = ["$scope", "$state", "AIPReviewService", "AIPUserService", "SpinnerService", "$timeout", "$q", "$uibModal", "APP_ROOT", "$sce", "$filter"];
             $scope.vm = this;
             this.$state = $state;
             this.aipReviewService = AIPReviewService;
@@ -19,13 +19,94 @@ var AIP;
             this.$uibModal = $uibModal;
             this.APP_ROOT = APP_ROOT;
             this.$sce = $sce;
+            this.$filter = $filter;
             this.actionItemNamesList = [];
             this.actionItemDetails = null;
             this.personId;
             this.personName;
             this.selected;
             this.option;
+            this.showModal = false;
             this.init();
+            $scope.header = [{
+                    name: "id",
+                    title: "ID",
+                    width: "0px",
+                    options: {
+                        sortable: false,
+                        visible: false,
+                        columnShowHide: false
+                    }
+                }, {
+                    name: "attachmentName",
+                    title: $filter("i18n_aip")("js.aip.review.monitor.action.viewAttachments.modal.grid.header.attachmentName"),
+                    ariaLabel: $filter("i18n_aip")("js.aip.review.monitor.action.viewAttachments.modal.grid.header.attachmentName"),
+                    width: "20%",
+                    options: {
+                        sortable: true,
+                        visible: true,
+                        ascending: true,
+                        columnShowHide: false
+                    }
+                }, {
+                    name: "uploadedDate",
+                    title: $filter("i18n_aip")("js.aip.review.monitor.action.viewAttachments.modal.grid.header.uploadDate"),
+                    ariaLabel: $filter("i18n_aip")("js.aip.review.monitor.action.viewAttachments.modal.grid.header.uploadDate"),
+                    width: "20%",
+                    options: {
+                        sortable: true,
+                        visible: true,
+                        columnShowHide: false
+                    }
+                },
+                {
+                    name: "location",
+                    title: $filter("i18n_aip")("js.aip.review.monitor.action.viewAttachments.modal.grid.header.location"),
+                    ariaLabel: $filter("i18n_aip")("js.aip.review.monitor.action.viewAttachments.modal.grid.header.location"),
+                    width: "20%",
+                    options: {
+                        sortable: true,
+                        visible: true,
+                        columnShowHide: false
+                    }
+                },
+                {
+                    name: "comments",
+                    title: $filter("i18n_aip")("js.aip.review.monitor.action.viewAttachments.modal.grid.header.comments"),
+                    ariaLabel: $filter("i18n_aip")("js.aip.review.monitor.action.viewAttachments.modal.grid.header.comments"),
+                    width: "20%",
+                    options: {
+                        sortable: true,
+                        visible: true,
+                        columnShowHide: false
+                    }
+                },
+                {
+                    name: "attachmentActions",
+                    title: $filter("i18n_aip")("js.aip.attachments.grid.header.actions"),
+                    ariaLabel: $filter("i18n_aip")("js.aip.attachments.grid.header.actions"),
+                    width: "20%",
+                    options: {
+                        sortable: false,
+                        visible: true,
+                        columnShowHide: false
+                    }
+                }
+            ];
+            $scope.fetchData = function (query) {
+                var deferred = $q.defer();
+                query.actionItemId = this.actionItemDetails.actionItemId;
+                query.responseId = this.actionItemDetails.responseId;
+                $scope.query = query;
+                AIPUploadService.fetchAttachmentsList(query)
+                    .then(function (response) {
+                    deferred.resolve(response);
+                }, function (error) {
+                    console.log(error);
+                    deferred.reject(error);
+                });
+                return deferred.promise;
+            };
         }
         ReviewActionItemCtrl.prototype.init = function () {
             var _this = this;
@@ -35,9 +116,11 @@ var AIP;
                 _this.actionItemDetails = response.data;
             }));
         };
+        ReviewActionItemCtrl.prototype.viewAttachemnts = function () {
+            this.showModal = true;
+        };
         return ReviewActionItemCtrl;
-    })();
+    }());
     AIP.ReviewActionItemCtrl = ReviewActionItemCtrl;
 })(AIP || (AIP = {}));
 register("bannerAIPReview").controller("reviewActionItemCtrl", AIP.ReviewActionItemCtrl);
-//# sourceMappingURL=reviewActionItemCtrl.js.map
--- web-app/aipApp/aipReview/reviewActionItem/reviewActionItemCtrl.ts
+++ web-app/aipApp/aipReview/reviewActionItem/reviewActionItemCtrl.ts
@@ -17,7 +17,7 @@ module AIP {
 
     export class ReviewActionItemCtrl implements IReviewActionItemCtrl {
 
-        $inject = ["$scope", "$state", "AIPReviewService", "AIPUserService", "SpinnerService", "$timeout", "$q", "$uibModal", "APP_ROOT", "$sce"];
+        $inject = ["$scope", "$state", "AIPReviewService", "AIPUserService", "SpinnerService", "$timeout", "$q", "$uibModal", "APP_ROOT", "$sce","$filter"];
         aipReviewService: AIP.AIPReviewService;
         userService: AIP.UserService;
         actionItems: IActionItem[];
@@ -26,6 +26,7 @@ module AIP {
         $sce;
         $timeout;
         $state;
+        $filter;
         $q;
         APP_ROOT;
         actionItemNamesList;
@@ -34,8 +35,9 @@ module AIP {
         personName;
         selected;
         option;
+        showModal;
 
-        constructor($scope, $state, AIPReviewService, AIPUserService, SpinnerService, $timeout, $q, $uibModal, APP_ROOT, $sce) {
+        constructor($scope, $state, AIPReviewService, AIPUserService, SpinnerService, $timeout, $q, $uibModal, APP_ROOT, $sce, $filter) {
             $scope.vm = this;
             this.$state = $state;
             this.aipReviewService = AIPReviewService;
@@ -46,13 +48,95 @@ module AIP {
             this.$uibModal = $uibModal;
             this.APP_ROOT = APP_ROOT;
             this.$sce = $sce;
+            this.$filter = $filter;
             this.actionItemNamesList=[];
             this.actionItemDetails = null;
             this.personId;
             this.personName;
             this.selected;
             this.option;
+            this.showModal = false;
             this.init();
+            $scope.header = [{
+                           name: "id",
+                           title: "ID",
+                           width: "0px",
+                           options: {
+                               sortable: false,
+                               visible: false,
+                               columnShowHide: false
+                           }
+                       }, {
+                           name: "attachmentName",
+                           title: $filter("i18n_aip")("js.aip.review.monitor.action.viewAttachments.modal.grid.header.attachmentName"),
+                           ariaLabel: $filter("i18n_aip")("js.aip.review.monitor.action.viewAttachments.modal.grid.header.attachmentName"),
+                           width: "20%",
+                           options: {
+                               sortable: true,
+                               visible: true,
+                               ascending: true,
+                               columnShowHide: false
+                           }
+                       }, {
+                           name: "uploadedDate",
+                           title: $filter("i18n_aip")("js.aip.review.monitor.action.viewAttachments.modal.grid.header.uploadDate"),
+                           ariaLabel: $filter("i18n_aip")("js.aip.review.monitor.action.viewAttachments.modal.grid.header.uploadDate"),
+                           width: "20%",
+                           options: {
+                               sortable: true,
+                               visible: true,
+                               columnShowHide: false
+                           }
+                       },
+                {
+                                           name: "location",
+                                           title: $filter("i18n_aip")("js.aip.review.monitor.action.viewAttachments.modal.grid.header.location"),
+                                           ariaLabel: $filter("i18n_aip")("js.aip.review.monitor.action.viewAttachments.modal.grid.header.location"),
+                                           width: "20%",
+                                           options: {
+                                               sortable: true,
+                                               visible: true,
+                                               columnShowHide: false
+                                           }
+                                       },
+                {
+                                           name: "comments",
+                                           title: $filter("i18n_aip")("js.aip.review.monitor.action.viewAttachments.modal.grid.header.comments"),
+                                           ariaLabel: $filter("i18n_aip")("js.aip.review.monitor.action.viewAttachments.modal.grid.header.comments"),
+                                           width: "20%",
+                                           options: {
+                                               sortable: true,
+                                               visible: true,
+                                               columnShowHide: false
+                                           }
+                                       },
+                           {
+                               name: "attachmentActions",
+                               title: $filter("i18n_aip")("js.aip.attachments.grid.header.actions"),
+                               ariaLabel: $filter("i18n_aip")("js.aip.attachments.grid.header.actions"),
+                               width: "20%",
+                               options: {
+                                   sortable: false,
+                                   visible: true,
+                                   columnShowHide: false
+                               }
+                           }
+                       ];
+            $scope.fetchData = function (query: AIP.IAttachmentListQuery) {
+
+                            var deferred = $q.defer();
+                            query.actionItemId = this.actionItemDetails.actionItemId;
+                            query.responseId = this.actionItemDetails.responseId;
+                            $scope.query = query;
+                            AIPUploadService.fetchAttachmentsList(query)
+                                .then((response) => {
+                                    deferred.resolve(response);
+                                }, (error) => {
+                                    console.log(error);
+                                    deferred.reject(error);
+                                });
+                            return deferred.promise;
+                        };
         }
 
         init() {
@@ -63,7 +147,11 @@ module AIP {
                         this.actionItemDetails = response.data;
                     })
             );
+
         }
+        viewAttachemnts(){
+            this.showModal = true;
+         }
     }
 }
 
