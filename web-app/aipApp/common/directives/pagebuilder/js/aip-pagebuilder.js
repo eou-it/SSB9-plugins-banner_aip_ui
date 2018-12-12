6006298b8e123525dda3e092f16bea7718e9e9bd
S, Anitha <Anitha.S@ellucian.com>
2018-12-12 14:44:39 +0530

    BS-10099 loading multiple Action items under same group issue is fixed

--- web-app/aipApp/common/directives/pagebuilder/js/aip-pagebuilder.js
+++ web-app/aipApp/common/directives/pagebuilder/js/aip-pagebuilder.js
@@ -4,7 +4,7 @@
 ///<reference path="../../../../../typings/tsd.d.ts"/>
 var PB;
 (function (PB) {
-    var PageBuilderPage = (function () {
+    var PageBuilderPage = /** @class */ (function () {
         function PageBuilderPage($compile, ItemListViewService) {
             this.restrict = "AE";
             this.transclude = true;
@@ -22,39 +22,41 @@ var PB;
         };
         PageBuilderPage.prototype.link = function (scope, element, attrs) {
             var self = this;
-            attrs.$observe('aid', function (tpl) {
+            scope.getPagebuilderDetails = function (tpl) {
                 self.ItemListViewService.getPagebuilderPage(attrs.page, attrs.aid, attrs.gid)
                     .then(function (val) {
-                    element.children().empty();
-                    var tempElement = angular.element(val.html);
-                    var bodyContent = tempElement.filter("div.customPage").attr("id", "PBContent");
-                    var aipController = {};
-                    var pbController = "CustomPageController_" + attrs.page;
-                    var appModule = appModule || angular.module('BannerOnAngular');
-                    appModule.requires.push('ngResource', 'ngGrid', 'ui', 'pbrun.directives', 'ngSanitize', 'xe-ui-components');
-                    /* disable debug: */
-                    appModule.config(['$compileProvider', function ($compileProvider) {
+                        element.children().empty();
+                        var tempElement = angular.element(val.html);
+                        var bodyContent = tempElement.filter("div.customPage").attr("id", "PBContent");
+                        var aipController = {};
+                        var pbController = "CustomPageController_" + attrs.page;
+                        var appModule = appModule || angular.module('BannerOnAngular');
+                        appModule.requires.push('ngResource', 'ngGrid', 'ui', 'pbrun.directives', 'ngSanitize', 'xe-ui-components');
+                        /* disable debug: */
+                        appModule.config(['$compileProvider', function ($compileProvider) {
                             $compileProvider.debugInfoEnabled(false);
                         }]);
-                    if (window[pbController]) {
-                        aipController[pbController] = window[pbController];
-                    }
-                    for (var pc in aipController) {
-                        if (aipController.hasOwnProperty(pc)) {
-                            appModule.controller(pc, aipController[pc]);
+                        if (window[pbController]) {
+                            aipController[pbController] = window[pbController];
                         }
-                    }
-                    angular.bootstrap(bodyContent, ["BannerOnAngular"]);
-                    element.append(bodyContent);
-                });
-            });
+                        for (var pc in aipController) {
+                            if (aipController.hasOwnProperty(pc)) {
+                                appModule.controller(pc, aipController[pc]);
+                            }
+                        }
+                        angular.bootstrap(bodyContent, ["BannerOnAngular"]);
+                        element.append(bodyContent);
+                    });
+            };
+            scope.$watch(function () {
+                return [attrs.aid, attrs.page];
+            }, scope.getPagebuilderDetails, true);
         };
         PageBuilderPage.prototype.controller = function ($scope) {
         };
         PageBuilderPage.$inject = ["$compile", "ItemListViewService"];
         return PageBuilderPage;
-    })();
+    }());
     PB.PageBuilderPage = PageBuilderPage;
 })(PB || (PB = {}));
 register("BannerOnAngular").directive("pagebuilderPage", PB.PageBuilderPage);
-//# sourceMappingURL=aip-pagebuilder.js.map
