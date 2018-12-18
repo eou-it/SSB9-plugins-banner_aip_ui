1d02ff29dd05392daf4169d309098619efb66d32
sudhakaras <sudhakara.shivappa@ellucian.com>
2018-12-18 15:19:48 +0530

    NO JIRA fixed sonar issues

--- web-app/aipApp/admin/action/post/adminPostItemAddPageCtrl.js
+++ web-app/aipApp/admin/action/post/adminPostItemAddPageCtrl.js
@@ -1,3 +1,6 @@
+/*******************************************************************************
+ Copyright 2018 Ellucian Company L.P. and its affiliates.
+ ********************************************************************************/
 ///<reference path="../../../../typings/tsd.d.ts"/>
 ///<reference path="../../../common/services/spinnerService.ts"/>
 ///<reference path="../../../common/services/admin/adminActionService.ts"/>
