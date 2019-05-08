49ec32953e0d198fff38f00be4b81cb6f0b6b719
sivaramn <sivaram.nyapathi@ellucian.com>
2019-05-08 13:22:11 +0530

    BS-13127 AIP : Recurring Jobs : Change in Time does not reflect the equivalent Time on "Corresponding App Server Details" section.

--- web-app/aipApp/admin/action/post/adminPostItemAddPageCtrl.js
+++ web-app/aipApp/admin/action/post/adminPostItemAddPageCtrl.js
@@ -9,6 +9,7 @@ var AIP;
     var AdminPostItemAddPageCtrl = (function () {
         function AdminPostItemAddPageCtrl($scope, $rootScope, $q, $state, $uibModal, $filter, $timeout, SpinnerService, APP_ROOT, AdminActionStatusService, AdminActionService) {
             this.$inject = ["$scope", "$rootScope", "$q", "$state", "$filter", "$timeout", "SpinnerService", "AdminActionStatusService", "AdminActionService", "$uibModal", "APP_ROOT", "datePicker"];
+            this.END_OF_DAY = "2359";
             $scope.vm = this;
             this.$q = $q;
             this.$scope = $scope;
@@ -299,17 +300,34 @@ var AIP;
                 _this.appServerTimeZone = angular.element('<div></div>').html(serverTimeZone[serverTimeZone.length - 1]).text();
             });
         };
-        AdminPostItemAddPageCtrl.prototype.getProcessedServerRecuranceEndDate = function () {
+        AdminPostItemAddPageCtrl.prototype.getProcessedServerRecurranceEndDate = function () {
             var _this = this;
             var userSelectedVal = {
                 "userEnterDate": this.recurranceEndDate,
-                "userEnterTime": "2359",
+                "userEnterTime": this.END_OF_DAY,
                 "userEnterTimeZone": this.timezone.timezoneId
             };
             this.adminActionStatusService.getProcessedServerDateTimeAndTimezone(userSelectedVal)
                 .then(function (response) {
                 _this.processedServerDetails = response.data;
                 _this.serverRecurEndDate = _this.processedServerDetails.serverDate;
+            });
+        };
+        AdminPostItemAddPageCtrl.prototype.getProcessedServerRecurranceStartDate = function () {
+            var _this = this;
+            this.timeConversion();
+            var userSelectedVal = {
+                "userEnterDate": this.recurranceStartDate,
+                "userEnterTime": this.$filter("date")(this.recurrTime, "HHmm"),
+                "userEnterTimeZone": this.timezone.timezoneId
+            };
+            this.adminActionStatusService.getProcessedServerDateTimeAndTimezone(userSelectedVal)
+                .then(function (response) {
+                _this.processedServerDetails = response.data;
+                _this.serverRecurrStartDate = _this.processedServerDetails.serverDate;
+                _this.serverRecurStartTime = _this.processedServerDetails.serverTime;
+                var recurserverTimeZone = _this.processedServerDetails.serverTimeZone.split(" ");
+                _this.serverRecurTimeZone = angular.element('<div></div>').html(recurserverTimeZone[recurserverTimeZone.length - 1]).text();
             });
         };
         AdminPostItemAddPageCtrl.prototype.editPage = function () {
@@ -530,10 +548,12 @@ var AIP;
                     userSelectedTime = this.recurrTime;
                 }
                 this.displayDatetimeZone.dateVal = this.postActionItemInfo.scheduledStartDate;
-                this.displayDatetimeZone.timeVal = this.selectedTime;
+                this.displayDatetimeZone.timeVal = userSelectedTime;
                 this.displayDatetimeZone.timeZoneVal = this.timezone.stringOffset + ' ' + this.timezone.timezoneId;
             }
             if (this.scheduleType === 'RECUR') {
+                this.postActionItemInfo.scheduledStartDate = this.recurranceStartDate;
+                this.enteredDate = this.recurranceStartDate;
                 this.adminActionService.saveRecurringActionItem(this.postActionItemInfo, this.selected, this.modalResults, this.selectedPopulation, this.regeneratePopulation, this.recurCount, this.selectedRecurFrequency, this.displayStartDateOffset, this.recDisplayEndDateType, this.displayEndDateOffset, this.recurDisplayEndDate, this.recurranceStartDate, this.recurranceEndDate, userSelectedTime, this.timezone.timezoneId, this.displayDatetimeZone)
                     .then(function (response) {
                     _this.saving = false;
@@ -593,3 +613,4 @@ var AIP;
     AIP.AdminPostItemAddPageCtrl = AdminPostItemAddPageCtrl;
 })(AIP || (AIP = {}));
 register("bannerAIP").controller("AdminPostItemAddPageCtrl", AIP.AdminPostItemAddPageCtrl);
+//# sourceMappingURL=adminPostItemAddPageCtrl.js.map
