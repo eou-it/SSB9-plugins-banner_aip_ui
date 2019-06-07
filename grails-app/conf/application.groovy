3dfe06420bfeef424f2abf9813142fff9daad62e
sudhakaras <sudhakara.shivappa@ellucian.com>
2019-06-07 11:44:45 +0530

    BS-13232 application config update

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -16,39 +16,29 @@ import net.hedtech.banner.configuration.ApplicationConfigurationUtils as ConfigF
 // 2. Load the configuration file if it exists within the user's .grails directory (i.e., convenient for developers)
 // 3. Load the configuration file if its location was specified as a system environment variable
 //
-// Map [ environment variable or -D command line argument name : file path ]
-grails.config.locations = [] // leave this initialized to an empty list, and add your locations
-// in the APPLICATION CONFIGURATION section below.
-def locationAdder = ConfigFinder.&addLocation.curry( grails.config.locations )
+// Added for integration tests to run in plugin level
+grails.config.locations = [
+        BANNER_APP_CONFIG: "banner_configuration.groovy",
+        BANNER_GENERAL_SSB_CONFIG: "BannerGeneralSsb_configuration.groovy"
+]
 
-[BANNER_APP_CONFIG           : "banner_configuration.groovy",
- WEB_APP_EXTENSIBILITY_CONFIG: "WebAppExtensibilityConfig.class"
-].each {envName, defaultFileName -> locationAdder( envName, defaultFileName )}
-//[bannerGrailsAppConfig: "${userHome}/.grails/banner_configuration.groovy",
-//        customRepresentationConfig: "grails-app/conf/CustomRepresentationConfig.groovy",
-//].each { envName, defaultFileName -> locationAdder(envName, defaultFileName) }
-
-grails.databinding.useSpringBinder = true
-// You must create a small configuration file named 'banner_on_grails-local-config.groovy' (as referenced
-// in the above map defining externalized configuration files) that contains your own specific
-// configuration (e.g., URIs, usernames, etc.).  Following is an example of this file:
-
+grails.databinding.useSpringBinder=true
 
 grails.project.groupId = "net.hedtech" // used when deploying to a maven repo
 
 grails.mime.file.extensions = true // enables the parsing of file extensions from URLs into the request format
 grails.mime.use.accept.header = false
-grails.mime.types = [html         : ['text/html', 'application/xhtml+xml'],
-                     xml          : ['text/xml', 'application/xml', 'application/vnd.sungardhe.student.v0.01+xml'],
-                     text         : 'text/plain',
-                     js           : 'text/javascript',
-                     rss          : 'application/rss+xml',
-                     atom         : 'application/atom+xml',
-                     css          : 'text/css',
-                     csv          : 'text/csv',
-                     all          : '*/*',
-                     json         : ['application/json', 'text/json'],
-                     form         : 'application/x-www-form-urlencoded',
+grails.mime.types = [html: ['text/html', 'application/xhtml+xml'],
+                     xml: ['text/xml', 'application/xml', 'application/vnd.sungardhe.student.v0.01+xml'],
+                     text: 'text/plain',
+                     js: 'text/javascript',
+                     rss: 'application/rss+xml',
+                     atom: 'application/atom+xml',
+                     css: 'text/css',
+                     csv: 'text/csv',
+                     all: '*/*',
+                     json: ['application/json', 'text/json'],
+                     form: 'application/x-www-form-urlencoded',
                      multipartForm: 'multipart/form-data'
 ]
 
@@ -56,32 +46,10 @@ grails.mime.types = [html         : ['text/html', 'application/xhtml+xml'],
 grails.views.default.codec = "html" // none, html, base64  **** Charlie note: Setting this to html will ensure html is escaped, to prevent XSS attack ****
 grails.views.gsp.encoding = "UTF-8"
 grails.converters.encoding = "UTF-8"
-grails.databinding.useSpringBinder = true
+
 grails.converters.domain.include.version = true
 grails.converters.json.date = "javascript"
 grails.converters.json.pretty.print = true
-
-environments {
-    development {
-        grails.resources.debug = true
-    }
-    production {
-
-    }
-    test {
-        ssbEnabled = true
-        ssbOracleUsersProxied = true
-    }
-}
-
-banner {
-    sso {
-        authenticationProvider = 'default'
-        authenticationAssertionAttribute = 'udcId'
-    }
-}
-
-ssbEnabled = true
 
 // enabled native2ascii conversion of i18n properties files
 grails.enable.native2ascii = false
@@ -89,19 +57,27 @@ grails.enable.native2ascii = false
 // enable GSP preprocessing: replace head -> g:captureHead, title -> g:captureTitle, meta -> g:captureMeta, body -> g:captureBody
 grails.views.gsp.sitemesh.preprocess = true
 
-// ******************************************************************************
-//
-//                       +++ DATA ORIGIN CONFIGURATION +++
-//
-// ******************************************************************************
-// This field is a Banner standard, along with 'lastModifiedBy' and lastModified.
-// These properties are populated automatically before an entity is inserted or updated
-// within the database. The lastModifiedBy uses the username of the logged in user,
-// the lastModified uses the current timestamp, and the dataOrigin uses the value
-// specified here:
-dataOrigin = "Banner"
+dataSource {
+    dialect = "org.hibernate.dialect.Oracle10gDialect"
+    loggingSql = false
+}
 
+hibernate {
+    cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
+    cache.region_prefix = ''
+    //net.sf.ehcache.configurationResourceName = 'financeSSBAppEhCache.xml'
+    cache.region.factory_class = 'org.hibernate.cache.ehcache.SingletonEhCacheRegionFactory'
+    show_sql = false
+    dialect = "org.hibernate.dialect.Oracle10gDialect"
+    packagesToScan="net.hedtech"
+    config.location = [
+            "classpath:hibernate-banner-aip.cfg.xml",
+            "classpath:hibernate-banner-core.cfg.xml",
+            "classpath:hibernate-banner-general-common.cfg.xml",
+            "classpath:hibernate-banner-general-person.cfg.xml",
+            "classpath:hibernate-banner-general-validation-common.cfg.xml",
+            "classpath:hibernate-banner-general-utility.cfg.xml",
+            "classpath:hibernate-banner-sspb.cfg.xml"
+    ]
+}
 
-grails.plugin.springsecurity.useRequestMapDomainClass = false
-
-//grails.plugin.springsecurity.rejectIfNoRule = true
