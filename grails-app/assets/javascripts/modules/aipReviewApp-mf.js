7e03950eaef52e3269df049140648cbfe52b082e
S, Anitha <Anitha.S@ellucian.com>
2019-07-03 10:04:56 +0530

    No Jira Fixing dependencies

--- grails-app/assets/javascripts/modules/aipReviewApp-mf.js
+++ grails-app/assets/javascripts/modules/aipReviewApp-mf.js
@@ -1,8 +1,6 @@
-//= require modules/angularApp-mf.js
 //= require modules/commonComponents-mf.js
 //= require modules/aipCommon-mf.js
 //= require modules/aipReview-mf.js
-//= require modules/aipUitility-mf.js
 
 
 
