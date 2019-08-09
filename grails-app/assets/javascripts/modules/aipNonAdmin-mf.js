14c07089068265c85ef408e46a09bac66dab72e7
sudhakaras <sudhakara.shivappa@ellucian.com>
2019-08-09 17:57:09 +0530

    NO JIRA misplaced files moved to correct place.

--- grails-app/assets/javascripts/modules/aipNonAdmin-mf.js
+++ grails-app/assets/javascripts/modules/aipNonAdmin-mf.js
@@ -1,6 +1,4 @@
 //= require  aipApp/nonAdminApp.js
-//= require modules/extensibilityAngular-mf.js
-//= require modules/aipPB-mf.js
 //= require  aipApp/listItem/listItemPageCtrl.js
 //= require  aipApp/listItem/itemConfirm/itemConfirmCtrl.js
 //= require  aipApp/listItem/itemInform/itemInformCtrl.js
