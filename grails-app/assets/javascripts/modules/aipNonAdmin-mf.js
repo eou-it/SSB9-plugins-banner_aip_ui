76a708bdeb3ba8f64414935fa318ae5e9367395f
sudhakaras <sudhakara.shivappa@ellucian.com>
2019-08-09 18:43:19 +0530

    NO JIRA added page builder directive file nonadmin app

--- grails-app/assets/javascripts/modules/aipNonAdmin-mf.js
+++ grails-app/assets/javascripts/modules/aipNonAdmin-mf.js
@@ -2,3 +2,4 @@
 //= require  aipApp/listItem/listItemPageCtrl.js
 //= require  aipApp/listItem/itemConfirm/itemConfirmCtrl.js
 //= require  aipApp/listItem/itemInform/itemInformCtrl.js
+//= require  aipApp/common/directives/pagebuilder/js/aip-pagebuilder.js
--- grails-app/assets/javascripts/modules/aipNonAdminApp-mf.js
+++ grails-app/assets/javascripts/modules/aipNonAdminApp-mf.js
@@ -1,6 +1,6 @@
-//= require modules/aipCommon-mf.js
 //= require modules/extensibilityAngular-mf.js
 //= require modules/aipPB-mf.js
+//= require modules/aipCommon-mf.js
 //= require modules/aipNonAdmin-mf.js
 //= require modules/commonComponents-mf.js
 
