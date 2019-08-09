d71a0321ac4d7d9a3f52fea6974874613ecb9544
sudhakaras <sudhakara.shivappa@ellucian.com>
2019-08-09 20:07:29 +0530

    NO JIRA added aip admin common module file

---
+++ grails-app/assets/javascripts/aipApp/aipAdminCommon.js
@@ -0,0 +1,7 @@
+/*******************************************************************************
+ Copyright 2019 Ellucian Company L.P. and its affiliates.
+ ********************************************************************************/
+// angular module init and configuration
+"use strict";
+
+var BannerOnAngular = angular.module("BannerOnAngular", [])
--- grails-app/assets/javascripts/modules/aipAdminApp-mf.js
+++ grails-app/assets/javascripts/modules/aipAdminApp-mf.js
@@ -1,3 +1,4 @@
+//= require  aipApp/aipAdminCommon.js
 //= require modules/aipCommon-mf.js
 //= require modules/aipAdmin-mf.js
 //= require modules/commonComponents-mf.js
